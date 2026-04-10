e68af492624ff4626598c197f9d83a3c52e69093
ANUJ KUMAR YADAV <anujky919931@gmail.com>
2026-04-10 23:29:20 +0530

    Create Day2.py

---
+++ Day2.py
@@ -0,0 +1,11 @@
+# Variables matlab data store karne wale dabbe
+naam = "Anuj"
+college = "IIT Patna"
+course = "AICS"
+
+print("Mera naam " + naam + " hai.")
+print("Main " + college + " mein " + course + " padhta hoon.")
+
+# Number wale variables
+semester = 1
+print("Abhi mera semester", semester, "chal raha hai.")
